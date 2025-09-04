7daec88c85be92dad33a25dc0a259d00725042a8
Juan Esteban Barrios Tovar <jebarriost@eafit.edu.co>
2025-09-04 15:01:52 -0500

    login with redirect to upload

--- Data/views.py
+++ Data/views.py
@@ -27,7 +27,9 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 messages.success(request, f"¡Bienvenido, {username}!")
-                return redirect('home')
+                return redirect('/upload/')
+ 
+
             else:
                 messages.error(request, "Usuario o contraseña inválidos.")
         else:

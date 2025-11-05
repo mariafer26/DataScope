e4b4394cc4ef565707a76b0dd1f954eba9745289
fcastroj <fcastroj@eafit.edu.co>
2025-11-05 02:56:18 -0500

    Configuración final settings.py para Heroku

--- DataScope/settings.py
+++ DataScope/settings.py
@@ -70,19 +70,18 @@ WSGI_APPLICATION = "DataScope.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 if os.environ.get('DYNO'):
-    # Estamos en Heroku
+    # Producción en Heroku: usar PostgreSQL
     DATABASES = {
         'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 else:
-    # Local
+    # Local: usar SQLite
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / "db.sqlite3",
         }
     }
-
 
 
 # Password validation
